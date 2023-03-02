5a14ca93794a203158f61b8c66d214c3bcc8ef93
Miguel291185 <126734232+Miguel291185@users.noreply.github.com>
2023-03-01 23:59:51 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,25 @@
+
+    function mostrar(){
+    document.getElementById('lista').style.display = 'block' ;
+}
+
+function ocultar(){
+    document.getElementById('lista').style.display = 'none' ;
+}
+
+function mostrar1(){
+    document.getElementById('lista1').style.display = 'block' ;
+}
+
+function ocultar1(){
+    document.getElementById('lista1').style.display = 'none' ;
+}
+
+function mostrar2(){
+    document.getElementById('lista2').style.display = 'block' ;
+}
+
+function ocultar2(){
+    document.getElementById('lista2').style.display = 'none' ;
+}
+
